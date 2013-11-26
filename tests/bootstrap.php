513be0615d32a1960c47b63365772e28e79d313f
Kir Melnikov <kir@melniki.pro>
2013-11-27 04:48:42 +0600

    unit test fix

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -9,4 +9,4 @@
 $yiit = dirname(__FILE__) . '/../../../../../yii/framework/yiit.php';
 $config = dirname(__FILE__) . '/../../../config/test.php';
 require_once($yiit);
-Yii::createWebApplication($config);
+//Yii::createWebApplication($config);
