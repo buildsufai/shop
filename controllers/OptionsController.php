c0da9f48f82609efdd5c9e2242595b80ba795b41
Kir Melnikov <kir@melniki.pro>
2013-12-18 20:41:25 +0600

    options and product

---
+++ controllers/OptionsController.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * User: Kir Melnikov
+ * Date: 18.12.13
+ * Time: 20:11
+ *
+ */
+
+class OptionsController extends Controller
+{
+
+    public function actionIndex()
+    {
+        echo 'options';
+    }
+} 
