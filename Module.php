b042ba640b479f6a44f2ef2bd6e23cc3b1378920
Kir Melnikov <kir@melniki.pro>
2013-12-31 02:39:23 +0600

    Yii2 module class

---
+++ Module.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * User: Kir Melnikov
+ * Date: 19.12.13
+ * Time: 19:33
+ *
+ */
+namespace shop;
+
+class Module extends \yii\base\Module {
+
+    public $controllerNamespace = 'shop\controllers';
+
+    /**
+     * Returns the module version number.
+     * @return string the version.
+     */
+    public function getVersion()
+    {
+        return '1.0.0';
+    }
+} 
